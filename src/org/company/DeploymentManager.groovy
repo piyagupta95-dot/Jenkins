80e5352ee61130e5e90b4c65283b0213405bfc75
piyagupta95-dot <piyagupta95@gmail.com>
2026-05-28 19:00:07 +0530

    Update DeploymentManager.groovy

--- src/org/company/DeploymentManager.groovy
+++ src/org/company/DeploymentManager.groovy
@@ -1,1 +1,28 @@
+package org.company
+
+class DeploymentManager implements Serializable {
+    def script
+    String environment
+
+   
+    DeploymentManager(def script, String environment) {
+        this.script = script
+        this.environment = environment
+    }
+
+    void validate() {
+        
+        script.echo "Step 1: Validating configuration for the [${environment}] environment..."
+    }
+
+    void deploy() {
+        script.echo "Step 2: Deploying application to [${environment}]..."
+        script.echo "Success: [${environment}] deployment is complete!"
+    }
+
+    void rollback() {
+        script.echo "ALERT: Something went wrong!"
+        script.echo "Rolling back the [${environment}] environment to the previous stable version..."
+    }
+}
 
